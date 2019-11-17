61c25f552e3c5138c168ac7595fbe145bcec9e6e
Ulderico Cirello <u@cirello.eu>
2019-11-17 09:02:36 -0800

    pgqueue: remove unnecessary error annotation

--- queue.go
+++ queue.go
@@ -179,10 +179,7 @@ func (c *Client) CreateTable() error {
 		CREATE INDEX IF NOT EXISTS ` + pq.QuoteIdentifier(c.tableName+"_pop") + ` ON ` + pq.QuoteIdentifier(c.tableName) + ` (queue, state);
 		CREATE INDEX IF NOT EXISTS ` + pq.QuoteIdentifier(c.tableName+"_vacuum") + ` ON ` + pq.QuoteIdentifier(c.tableName) + ` (queue, state, deliveries, leased_until);
 		`)
-		if err != nil {
-			return fmt.Errorf("cannot create queue table: %w", err)
-		}
-		return nil
+		return err
 	})
 }
 
